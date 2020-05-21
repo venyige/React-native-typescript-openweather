0ce903d1e7d421aaee7c1b96bba9ac6da994a222
Tibor Venyige <tibor@venyige.com>
2020-05-21 03:14:12 +0200

    Geolocation corrected, fallback to Budapest

--- components/geolocproc.tsx
+++ components/geolocproc.tsx
@@ -2,20 +2,20 @@ import React from 'react';
 import GetLocation from 'react-native-get-location';
 
 
-export const  GeoLoc =async():Promise<any> =>{
-    let Loc:any =null;
-        try {
-             Loc = await GetLocation.getCurrentPosition({
-                enableHighAccuracy: true,
-                timeout: 150000,
-            })
-console.log('Loc: '+Loc);
-return(Loc);
-        } catch (error) {
-            console.error('Geoloc error is ', error);
-Loc='';
-return(Loc);
-        }
+export const GeoLoc = async (): Promise<any> => {
+    let Loc: any = null;
+    try {
+        Loc = await GetLocation.getCurrentPosition({
+            enableHighAccuracy: true,
+            timeout: 5000,
+        })
+        console.log('Loc: ' + Loc);
+        return (Loc);
+    } catch (error) {
+        Loc = { "latitude": 47.5, "longitude": 19.04 };
+        console.log('geoLoc error: '+error+'; Loc fallback: ' + Loc);
+        return (Loc);
+    }
 
 }
 
